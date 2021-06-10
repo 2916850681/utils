2789040696a5a7bed785532da635a1e4293e2d49
2916850681 <44263056+2916850681@users.noreply.github.com>
2021-06-10 11:07:21 +0800

    Add files via upload

---
+++ tmp.py
@@ -0,0 +1,20 @@
+import glob
+from os.path import join
+
+root = '/home/dell/FeatureGroup/liyachuan/00Dataset/CurlingDetection/labels'
+dir = 'test', 'train', 'val'
+
+txt_file = join(root, '*', '*.txt')
+i = 0
+for path in glob.glob(txt_file):
+
+    with open(path, 'r') as f:
+        context = f.readlines()
+    for index in range(len(context)):
+        if context[index][0] == '2':
+            context[index] = '1' + context[index][1:]
+
+    with open(path, 'w') as f:
+        f.writelines(context)
+    print(i)
+    i+=1
